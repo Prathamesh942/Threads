044ba42bf4290915a49720e2222a047562cd3215
prathamesh <prathameshsk94@gmail.com>
2024-06-28 14:26:46 +0530

    add post model

---
+++ server/src/models/post.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const PostSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Like",
+      },
+    ],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Post = mongoose.model("Post", PostSchema);

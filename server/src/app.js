ed350c428e7e8603b836dab28f58fb9abef0f8c6
prathamesh <prathameshsk94@gmail.com>
2024-06-28 13:55:55 +0530

    connect to db

---
+++ server/src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", () => {
+  console.log(hii);
+});
+
+export { app };

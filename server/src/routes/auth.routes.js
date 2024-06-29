453c5225b62a4bf5311c6a826f1b91678e522a5b
prathamesh <prathameshsk94@gmail.com>
2024-06-29 16:34:48 +0530

    add file upload middleware

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -5,10 +5,13 @@ import {
   logoutUser,
 } from "../controller/auth.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router
+  .route("/register")
+  .post(upload.fields([{ name: "profile", maxCount: 1 }]), registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt, logoutUser);
 

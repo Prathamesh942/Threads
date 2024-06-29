507f8b222fb47a1abe6a9a0a116844bc9c4c95f5
prathamesh <prathameshsk94@gmail.com>
2024-06-29 16:35:43 +0530

    add other routes file

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import testRouter from "./routes/test.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", () => {
-  console.log(hii);
+app.get("/", (req, res) => {
+  console.log("hii");
+  res.json("hii");
 });
+
+app.use("/api/v1/test", testRouter);
+app.use("/api/v1/auth", authRouter);
 
 export { app };

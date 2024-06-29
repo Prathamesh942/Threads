8e5d13282f692882946259811c121a8f5fe90fc5
prathamesh <prathameshsk94@gmail.com>
2024-06-29 11:37:16 +0530

    add jwt

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new Schema(
   {
@@ -56,7 +57,7 @@ const UserSchema = new Schema(
   }
 );
 
-UserSchema.pre("save", async function () {
+UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
@@ -68,6 +69,20 @@ UserSchema.statics.doesNotExist = async function (field) {
 
 UserSchema.methods.comparePasswords = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
 };
 
 export const User = mongoose.model("User", UserSchema);

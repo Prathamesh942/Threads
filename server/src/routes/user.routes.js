7add05a83295a10d8e48ec9fd16dd284b3f6ce24
prathamesh <prathameshsk94@gmail.com>
2024-06-30 15:42:58 +0530

    init react project

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -9,7 +9,7 @@ import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/:userId").get(verifyJwt, getuser);
+router.route("/:userId").get(getuser);
 router.route("/:userId").put(verifyJwt, updateuser);
 router.route("/:userId/follow").post(verifyJwt, followuser);
 router.route("/:userId/unfollow").post(verifyJwt, unfollowuser);

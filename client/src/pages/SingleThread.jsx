489851191babfcd5ce464c19f7d9fa5910fc1313
prathamesh <prathameshsk94@gmail.com>
2024-07-04 16:09:29 +0530

    center

--- client/src/pages/SingleThread.jsx
+++ client/src/pages/SingleThread.jsx
@@ -199,7 +199,7 @@ const SingleThread = () => {
                         <h2 className=" flex items-center gap-1 text-zinc-300">
                           <Link
                             to={`/${reply?.user?.username}`}
-                            className="cursor-pointer flex gap-1"
+                            className="cursor-pointer flex gap-1 items-center"
                           >
                             {reply?.user?.username}
                             {reply?.user?.username == "prathamesh_94" ? (

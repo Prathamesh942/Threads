44e581fe704fe7faf98d469302d4c2f141e5ea79
prathamesh <prathameshsk94@gmail.com>
2024-07-25 10:56:56 +0530

    fix image upload

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -15,11 +15,7 @@ const router = Router();
 router.route("/:userId").get(getuser);
 router
   .route("/:userId")
-  .put(
-    verifyJwt,
-    upload.fields([{ name: "profile", maxCount: 1 }]),
-    updateuser
-  );
+  .put(upload.fields([{ name: "profile", maxCount: 1 }]), updateuser);
 router.route("/:userId/follow").post(verifyJwt, followuser);
 router.route("/:userId/unfollow").post(verifyJwt, unfollowuser);
 router.route("/:userId/activity").get(verifyJwt, getActivity);
